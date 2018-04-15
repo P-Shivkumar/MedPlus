f1ed99854c9adf4f8229b5081d1618142ebf473f
Shivkumar <shivkumarr.patil1997@gmail.com>
2018-04-16 00:20:51 +0530

    changed database password to root

--- PHP/signup.php
+++ PHP/signup.php
@@ -1,7 +1,7 @@
 
 
 <?php
-$con=mysqli_connect("localhost","root","k136616","Healthcare");
+$con=mysqli_connect("localhost","root","root","Healthcare");
 if (mysqli_connect_errno()){
 	echo "Failed to connect to MySQL: " . mysqli_connect_error();
 }
